a9cff71ab8e379f491a58150f2c507718a7733dc
Hbldsr <654272094@>
2021-12-23 14:14:59 +0800

    配置界面路由信息

--- 前端/front_end/src/router/index.js
+++ 前端/front_end/src/router/index.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
+import CourseInformation from '../components/CourseInformation'
+import CourseManagement from '../components/CourseManagement'
+import UnitContent from '../components/UnitContent'
+import UnitInformation from '../components/UnitInformation'
+import UnitTesting from '../components/UnitTesting'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
   routes: [
     { path: '/', redirect: '/Login' },
-    { path: '/Login', component: Login }
+    { path: '/Login', component: Login },
+    { path: '/CourseInformation', component: CourseInformation },
+    { path: '/CourseManagement', component: CourseManagement },
+    { path: '/UnitContent', component: UnitContent },
+    { path: '/UnitInformation', component: UnitInformation },
+    { path: '/UnitTesting', component: UnitTesting }
   ]
 })
