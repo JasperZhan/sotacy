be4779ce0e2c64132bdbd4f5d7c6be88a19d531d
Jasper <zhanhanjie123@163.com>
2021-12-23 22:52:55 +0800

    添加新建判断题功能

--- 后端/sotacy/src/main/java/cn/hzu/sotacy/controller/judgmentQuestion/JudgmentQuestionController.java
+++ 后端/sotacy/src/main/java/cn/hzu/sotacy/controller/judgmentQuestion/JudgmentQuestionController.java
@@ -1,9 +1,17 @@
 package cn.hzu.sotacy.controller.judgmentQuestion;
 
 
+import cn.hzu.sotacy.model.judgmentQuestion.JudgmentQuestion;
+import cn.hzu.sotacy.response.ApiRestResponse;
+import cn.hzu.sotacy.service.judgmentQuestion.JudgmentQuestionService;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
@@ -14,7 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-12-23
  */
 @RestController
-@RequestMapping("/sotacy/judgment-question")
+@RequestMapping("/judgmentQuestion")
 public class JudgmentQuestionController {
 
+    @Resource
+    JudgmentQuestionService judgmentQuestionService;
+
+    @ResponseBody
+    @RequestMapping("/add")
+    ApiRestResponse<JudgmentQuestion> addJudgmentQuestion(HttpServletRequest request, HttpServletResponse response) {
+        return judgmentQuestionService.addJudgmentQuestion(request, response);
+    }
 }
