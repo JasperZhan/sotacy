e21af459c8bdb73fc50f94ac00fa03f61ac5a13e
Hbldsr <654272094@>
2021-12-24 00:08:40 +0800

    注册部分element组件

--- 前端/front_end/src/plugins/element.js
+++ 前端/front_end/src/plugins/element.js
@@ -1,4 +1,22 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import {
+  Button,
+  Menu,
+  MenuItem,
+  RadioGroup,
+  RadioButton,
+  Submenu,
+  MenuItemGroup,
+  Row,
+  Col
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(RadioButton)
+Vue.use(RadioGroup)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(Row)
+Vue.use(Col)
