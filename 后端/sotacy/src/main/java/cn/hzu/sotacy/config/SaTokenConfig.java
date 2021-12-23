e3b2e80f87deb8420a14cadb7eee24a879480580
Jasper <zhanhanjie123@163.com>
2021-12-23 12:34:32 +0800

    添加Sa-Token配置
    添加路径访问拦截

---
+++ 后端/sotacy/src/main/java/cn/hzu/sotacy/config/SaTokenConfig.java
@@ -0,0 +1,24 @@
+package cn.hzu.sotacy.config;
+
+import cn.dev33.satoken.interceptor.SaRouteInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Jasper Zhan
+ * @date 2021年12月20日 21:54
+ */
+@Configuration
+public class SaTokenConfig implements WebMvcConfigurer {
+    // 注册拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册Sa-Token的路由拦截器
+        registry.addInterceptor(new SaRouteInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login")
+                .excludePathPatterns("/admin/**");
+    }
+}
+
