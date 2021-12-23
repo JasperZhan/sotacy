ef8b3a84e9f71338d5d48d815851f326c1d6979a
Hbldsr <654272094@>
2021-12-24 01:19:54 +0800

    给页面添加同样的头部样式

--- 前端/front_end/src/components/UnitTesting.vue
+++ 前端/front_end/src/components/UnitTesting.vue
@@ -1,6 +1,13 @@
 <!--单元测试界面-->
 <template>
-<div></div>
+<div>
+  <el-menu class="el-menu-demo"
+           background-color="#d9ecff">
+    <el-menu-item style="float: right;margin-top: 20px;">
+      <el-button type="primary" >退出登录</el-button>
+    </el-menu-item>
+  </el-menu>
+</div>
 </template>
 
 <script>
