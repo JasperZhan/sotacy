8b210f4c6faed60260f90470eca63ee70198634e
Jasper <zhanhanjie123@163.com>
2021-12-23 22:52:01 +0800

    添加新建课程单元功能

--- 后端/sotacy/src/main/java/cn/hzu/sotacy/controller/course/CourseUnitController.java
+++ 后端/sotacy/src/main/java/cn/hzu/sotacy/controller/course/CourseUnitController.java
@@ -1,9 +1,18 @@
 package cn.hzu.sotacy.controller.course;
 
 
+import cn.hzu.sotacy.model.course.CourseUnit;
+import cn.hzu.sotacy.response.ApiRestResponse;
+import cn.hzu.sotacy.service.course.CourseUnitService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
@@ -14,7 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-12-23
  */
 @RestController
-@RequestMapping("/sotacy/course-unit")
+@RequestMapping("//courseUnit")
 public class CourseUnitController {
 
+    @Resource
+    CourseUnitService courseUnitService;
+
+    @ResponseBody
+    @PostMapping("/add")
+    ApiRestResponse<CourseUnit> addCourseUnit(HttpServletRequest request, HttpServletResponse response) {
+        return courseUnitService.addCourseUnit(request, response);
+    }
 }
--- 后端/sotacy/src/main/java/cn/hzu/sotacy/service/course/CourseUnitService.java
+++ 后端/sotacy/src/main/java/cn/hzu/sotacy/service/course/CourseUnitService.java
@@ -1,7 +1,11 @@
 package cn.hzu.sotacy.service.course;
 
 import cn.hzu.sotacy.model.course.CourseUnit;
+import cn.hzu.sotacy.response.ApiRestResponse;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * <p>
@@ -13,4 +17,13 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface CourseUnitService extends IService<CourseUnit> {
 
+    /**
+     * 新建课程单元
+     * @author Jasper Zhan
+     * @date 2021/12/23 14:01
+     * @param request 请求
+     * @param response 回复
+     * @return cn.hzu.sotacy.response.ApiRestResponse<cn.hzu.sotacy.model.course.CourseUnit>
+     */
+    ApiRestResponse<CourseUnit> addCourseUnit(HttpServletRequest request, HttpServletResponse response);
 }
