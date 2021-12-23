deeb0e3bc4e3d006ace5d9a3621ae39300462188
Jasper <zhanhanjie123@163.com>
2021-12-23 22:51:07 +0800

    添加新建课程功能

--- 后端/sotacy/src/main/java/cn/hzu/sotacy/dao/course/CourseDao.java
+++ 后端/sotacy/src/main/java/cn/hzu/sotacy/dao/course/CourseDao.java
@@ -15,4 +15,12 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface CourseDao extends BaseMapper<Course> {
 
+    /**
+     * 添加课程并且放回主键
+     * @author Jasper Zhan
+     * @date 2021/12/23 12:16
+     * @param course 保存的课程
+     * @return java.lang.Integer
+     */
+    Integer addCourse(Course course);
 }
--- 后端/sotacy/src/main/java/cn/hzu/sotacy/service/impl/course/CourseServiceImpl.java
+++ 后端/sotacy/src/main/java/cn/hzu/sotacy/service/impl/course/CourseServiceImpl.java
@@ -2,13 +2,19 @@ package cn.hzu.sotacy.service.impl.course;
 
 import cn.hzu.sotacy.model.course.Course;
 import cn.hzu.sotacy.dao.course.CourseDao;
+import cn.hzu.sotacy.response.ApiRestResponse;
+import cn.hzu.sotacy.result.CodeResult;
 import cn.hzu.sotacy.service.course.CourseService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 作者
@@ -17,4 +23,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class CourseServiceImpl extends ServiceImpl<CourseDao, Course> implements CourseService {
 
+    @Resource
+    CourseDao courseDao;
+
+    /**
+     * 新建课程
+     *
+     * @param request  请求
+     * @param response 回复
+     * @return cn.hzu.sotacy.response.ApiRestResponse<cn.hzu.sotacy.model.course.Course>
+     * @author Jasper Zhan
+     * @date 2021/12/23 11:48
+     */
+    @Override
+    public ApiRestResponse<Course> addCourse(HttpServletRequest request, HttpServletResponse response) {
+
+        String title = request.getParameter("title");
+        String describe = request.getParameter("describe");
+
+        if (title == null) {
+            return ApiRestResponse.fail(CodeResult.EMPTY_COURSE_TITLE);
+        }
+
+        Course course = new Course();
+        course.setTitle(title);
+        course.setDescribe(describe);
+
+        try {
+            courseDao.addCourse(course);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ApiRestResponse.fail(CodeResult.FAIL_ADD_COURSE);
+        }
+
+        if (course.getId() == null)
+            return ApiRestResponse.fail(CodeResult.EMPTY_COURSE_ID);
+
+        return ApiRestResponse.success(CodeResult.SUCCESS_ADD_COURSE, course);
+    }
 }
